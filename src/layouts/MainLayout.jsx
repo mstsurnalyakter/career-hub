c3fb0dc5c8c0866979f4f7866bad240ca05d4908
mstsurnalyakter <surnalyms@gmail.com>
2024-03-22 22:10:57 +0600

    need file added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import NavBar from '../components/NavBar'
+import { Outlet } from 'react-router'
+import Footer from '../components/Footer'
 
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <div>
+        <div>
+            <NavBar/>
+        </div>
+        <div>
+            <Outlet/>
+        </div>
+        <Footer/>
+    </div>
   )
 }
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
 
-const Routes = () => {
-  return (
-    <div>Routes</div>
-  )
-}
-
-export default Routes
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<MainLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<Home/>
+            }
+        ]
+    }
+])
