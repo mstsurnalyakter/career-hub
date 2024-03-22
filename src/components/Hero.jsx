a5137e4ee6781cd4699feee4464c17bf4d6ffa04
mstsurnalyakter <surnalyms@gmail.com>
2024-03-23 00:15:40 +0600

    hero component added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,39 +1,33 @@
-import React from 'react'
 
+import userImage from "../assets/images/user.png";
 const Hero = () => {
   return (
     <section className="bg-gradient-to-r from-blue-50 to-purple-50 w-full mb-20">
-      <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+      <div className="container flex flex-col justify-center p-6  mx-auto lg:pt-10  lg:pb-1 lg:flex-row lg:justify-between">
         <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
           <h1 className="text-5xl font-bold leading-none sm:text-6xl">
-            Ac mattis
-            <span className="text-violet-400">senectus</span>erat pharetra
+            Step Closer To Your {" "}
+            <span className="bg-gradient-to-r from-primary via-purple-600 to-pink-300 bg-300% text-transparent bg-clip-text animate-gradient">
+              Dream Job
+            </span>
           </h1>
           <p className="mt-6 mb-8 text-lg sm:mb-12">
-            Dictum aliquam porta in condimentum ac integer
-            <br className="hidden md:inline lg:hidden" />
-            turpis pulvinar, est scelerisque ligula sem
+            Explore thousands of job opportunities with all the information you
+            need. Its your future. Come find it. Manage all your job application
+            from start to finish.
           </p>
           <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
             <a
-              rel="noopener noreferrer"
-              href="#"
-              className="px-8 py-3 text-lg font-semibold rounded bg-violet-400 text-gray-900"
+              href="#_"
+              className="px-4 py-3 text-xl font-semibold text-center text-white transition duration-300 rounded-lg hover:from-purple-600 hover:to-[#7E90FE] ease bg-gradient-to-br from-[#7E90FE] to-purple-700 md:w-auto"
             >
-              Suspendisse
-            </a>
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="px-8 py-3 text-lg font-semibold border rounded border-gray-100"
-            >
-              Malesuada
+              Get Started
             </a>
           </div>
         </div>
         <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
           <img
-            src="assets/svg/Business_SVG.svg"
+            src={userImage}
             alt=""
             className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
           />
