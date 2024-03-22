8fe967a041037605e2dc4ba8cd2aaa4dc3fa1fb9
mstsurnalyakter <surnalyms@gmail.com>
2024-03-23 03:52:25 +0600

    feature section dynamic value added

--- src/components/FeaturedJobs.jsx
+++ src/components/FeaturedJobs.jsx
@@ -1,8 +1,27 @@
 import accounts from "../assets/icons/accounts.png";
 import { CiLocationOn } from "react-icons/ci";
 import { AiOutlineDollar } from "react-icons/ai";
+import { useEffect, useState } from "react";
 
 const FeaturedJobs = () => {
+  const [jobs,setJobs] = useState([]);
+
+  useEffect(()=>{
+    const loadData = async () =>{
+      try {
+        const res = await fetch("./data/jobs.json")
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    loadData();
+  },[])
+
+  console.log(jobs);
+
+
   return (
     <section className="py-6 sm:py-12 mb-32">
       <div className="container p-6 mx-auto space-y-8 mb-10">
@@ -14,85 +33,80 @@ const FeaturedJobs = () => {
           </p>
         </div>
         <div className="grid grid-cols-1 gap-x-4 gap-y-8  lg:grid-cols-2">
-          <article className="flex flex-col bg-gradient-to-r from-blue-50 rounded-lg to-purple-50 p-10">
-            <figure className="bg-gradient-to-r from-blue-100 mb-4 flex items-center justify-center to-purple-100 bg-opacity-10 w-20 h-20 rounded-lg">
-              <img alt="" className="w-10 h-10" src={accounts} />
-            </figure>
-            <div className="flex flex-col">
-              <h3 className="flex-1 text-2xl mb-2 font-extrabold text-[#474747] leading-snug">
-                Technical Database Engineer
-              </h3>
-              <p className="text-[#757575] text-xl font-semibold mb-4">
-                Google LLC
-              </p>
-              <div className="space-x-3 mb-4">
-                <button
-                  type="button"
-                  className="px-4 py-2 font-semibold border border-[#7E90FE] border-dashed rounded text-[#7E90FE]"
+
+          {
+            jobs.map(job=>{
+              const {
+                id,
+                logo,
+                job_title,
+                company_name,
+                remote_or_onsite,
+                location,
+                job_type,
+                salary
+                // job_description,
+                // job_responsibility,
+                // educational_requirements,
+                // experiences,
+                // contact_information,
+                // phone,
+                // email,
+                // address,
+              } = job;
+              return (
+                <article
+                  key={id}
+                  className="flex flex-col bg-gradient-to-r from-blue-50 rounded-lg to-purple-50 p-10"
                 >
-                  Remote
-                </button>
-                <button
-                  type="button"
-                  className="px-4 py-2 font-semibold border border-[#7E90FE] border-dashed rounded text-[#7E90FE]"
-                >
-                  Full Time
-                </button>
-              </div>
-              <div className="flex gap-6 mb-6">
-                <p className="flex items-center gap-2">
-                  <CiLocationOn />
-                  <span className="text-[#757575] text-xl font-semibold">
-                    Dhaka, Bangladesh
-                  </span>
-                </p>
-                <p className="flex items-center gap-2">
-                  <AiOutlineDollar />
-                  <span className="text-[#757575] text-xl font-semibold">
-                    Salary : 100K - 150K
-                  </span>
-                </p>
-              </div>
-              <div>
-                <button className="px-4 py-3 text-xl font-semibold text-center text-white transition duration-300 rounded-lg hover:from-purple-600 hover:to-[#7E90FE] ease bg-gradient-to-br from-[#7E90FE] to-purple-700 md:w-auto">
-                  View Details
-                </button>
-              </div>
-            </div>
-          </article>
-          <article className="flex flex-col bg-gradient-to-r from-blue-50 to-purple-50">
-            <figure className="bg-gradient-to-r mt-10 ml-10 from-blue-100 flex items-center justify-center to-purple-100 bg-opacity-10 w-16 h-16">
-              <img alt="" className="w-10 h-10" src={accounts} />
-            </figure>
-            <div className="flex flex-col p-6">
-              <h3 className="flex-1 text-lg font-semibold leading-snug">
-                Account & Finance
-              </h3>
-              <p>300 Jobs Available</p>
-            </div>
-          </article>
-          <article className="flex flex-col bg-gradient-to-r from-blue-50 to-purple-50">
-            <figure className="bg-gradient-to-r mt-10 ml-10 from-blue-100 flex items-center justify-center to-purple-100 bg-opacity-10 w-16 h-16">
-              <img alt="" className="w-10 h-10" src={accounts} />
-            </figure>
-            <div className="flex flex-col p-6">
-              <h3 className="flex-1 text-lg font-semibold leading-snug">
-                Account & Finance
-              </h3>
-              <p>300 Jobs Available</p>
-            </div>
-          </article>
-          <article className="flex flex-col bg-gradient-to-r from-blue-50 to-purple-50">
-            <figure className="bg-gradient-to-r mt-10 ml-10 from-blue-100 flex items-center justify-center to-purple-100 bg-opacity-10 w-16 h-16">
-              <img alt="" className="w-10 h-10" src={accounts} />
-            </figure>
-            <div className="flex flex-col p-6">
-              <h3 className="flex-1 text-lg font-semibold leading-snug">
-                Account & Finance
-              </h3>
-              <p>300 Jobs Available</p>
-            </div>
-          </article>
+                  <figure className="bg-gradient-to-r from-blue-100 mb-4 flex items-center justify-center to-purple-100 bg-opacity-10 w-28 h-20 rounded-lg">
+                    <img alt="" className="w-20 h-10" src={logo} />
+                  </figure>
+                  <div className="flex flex-col">
+                    <h3 className="flex-1 text-2xl mb-2 font-extrabold text-[#474747] leading-snug">
+                      {job_title}
+                    </h3>
+                    <p className="text-[#757575] text-xl font-semibold mb-4">
+                      {company_name}
+                    </p>
+                    <div className="space-x-3 mb-4">
+                      <button
+                        type="button"
+                        className="px-4 py-2 font-semibold border border-[#7E90FE] border-dashed rounded text-[#7E90FE]"
+                      >
+                       {remote_or_onsite}
+                      </button>
+                      <button
+                        type="button"
+                        className="px-4 py-2 font-semibold border border-[#7E90FE] border-dashed rounded text-[#7E90FE]"
+                      >
+                       {job_type}
+                      </button>
+                    </div>
+                    <div className="flex gap-6 mb-6">
+                      <p className="flex items-center gap-2">
+                        <CiLocationOn />
+                        <span className="text-[#757575] text-xl font-semibold">
+                         {location}
+                        </span>
+                      </p>
+                      <p className="flex items-center gap-2">
+                        <AiOutlineDollar />
+                        <span className="text-[#757575] text-xl font-semibold">
+                          Salary : {salary}
+                        </span>
+                      </p>
+                    </div>
+                    <div>
+                      <button className="px-4 py-3 text-xl font-semibold text-center text-white transition duration-300 rounded-lg hover:from-purple-600 hover:to-[#7E90FE] ease bg-gradient-to-br from-[#7E90FE] to-purple-700 md:w-auto">
+                        View Details
+                      </button>
+                    </div>
+                  </div>
+                </article>
+              );
+            })
+          }
         </div>
       </div>
       <div className="flex items-center justify-center">
